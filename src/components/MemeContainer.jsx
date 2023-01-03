53d11e52d7838dab0d360243ee4e898eeba1fb9b
avinashowgre <avinashow.gre@gmail.com>
2023-01-03 17:38:58 -0600

    Added memepreview and memecontainer component

--- src/components/MemeContainer.jsx
+++ src/components/MemeContainer.jsx
@@ -1,31 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import MemeUploader from "./MemeUploader";
+import MemePreview from "./MemePreview";
 
 export default function MemeContainer() {
   const [imgUrl, setImgUrl] = useState();
   const [captions, setCaptions] = useState([]);
+  const canvasRef = useRef();
+
+  function handleCaptionChange(index, caption) {
+    const captionsCpy = [...captions];
+
+    captionsCpy[index] = caption;
+
+    setCaptions(captionsCpy);
+  }
 
   return (
-    <div className="meme-container">
-      <div className="row" style={{ display: "flex", flexDirection: "row" }}>
-        <div className="col">
-          <p>Input goes here</p>
-          <div
-            className="meme-preview-container"
-            style={{
-              border: "1px solid black",
-              height: 500,
-              position: "relative",
-              width: 500,
-            }}
-          >
-            {imgUrl ? <p>sample</p> : <MemeUploader onFileInput={setImgUrl} />}
-          </div>
+    <div className="container mx-auto h-screen">
+      <div className="flex flex-row h-full">
+        <div className="w-2/3">
+          {imgUrl ? (
+            <MemePreview
+              captions={captions}
+              imageUrl={imgUrl}
+              onCaptionChange={handleCaptionChange}
+              ref={canvasRef}
+            />
+          ) : (
+            <MemeUploader onFileInput={setImgUrl} />
+          )}
         </div>
-        <div className="col">
-          <p>column2</p>
-        </div>
+        <div className="w-1/3 ">column2</div>
       </div>
     </div>
   );
--- src/components/MemePreview.jsx
+++ src/components/MemePreview.jsx
@@ -1,5 +1,94 @@
-import React from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 
-export default function MemePreview() {
-  return <></>;
+import { Bitmap, Stage, Text } from "@createjs/easeljs";
+
+const TEST_ID = "meme-preview";
+
+function MemePreview(props, ref) {
+  const { imageUrl, onCaptionChange, captions } = props;
+  const [canvasStage, setCanvasStage] = useState();
+
+  useEffect(() => {
+    setCanvasStage(new Stage(TEST_ID));
+  }, []);
+
+  useEffect(() => {
+    if (!canvasStage) return;
+
+    draw();
+  }, [imageUrl, captions, canvasStage]);
+
+  function drag(evt) {
+    evt.target.x = evt.stageX;
+    evt.target.y = evt.stageY;
+    evt.target.cursor = "pointer";
+    canvasStage.update();
+  }
+
+  function drop(evt, index) {
+    if (onCaptionChange) {
+      const { text, x, y } = evt.target;
+      onCaptionChange(index, {
+        ...texts[index],
+        text,
+        x,
+        y,
+      });
+    }
+  }
+
+  function draw() {
+    canvasStage.removeAllChildren();
+
+    const image = new Image();
+
+    image.onload = () => {
+      var bitmap = new Bitmap(image);
+      bitmap.scaleX = ref.current.width / image.width;
+      bitmap.scaleY = ref.current.height / image.height;
+
+      canvasStage.addChild(bitmap);
+
+      captions.forEach((caption, index) => {
+        const { color, font, fontFamily, fontSize, text, x, y } = caption;
+        let createText = new Text();
+        createText.set({
+          color,
+          text,
+          font: `${font} ${fontSize}px ${fontFamily}`,
+          lineWidth: canvasStage.canvas.width,
+          lineHeight: 20 * index,
+          textBaseline: "top",
+          textAlign: "left",
+          x,
+          y,
+        });
+        createText.on("pressmove", drag);
+        createText.on("pressup", function (evt) {
+          drop(evt, index);
+        });
+        canvasStage.addChild(createText);
+      });
+
+      canvasStage.update();
+    };
+
+    image.src = imageUrl;
+  }
+
+  return (
+    <canvas
+      ref={ref}
+      height="600"
+      id={TEST_ID}
+      style={{
+        border: "1px solid black",
+        maxWidth: "100%",
+        height: "auto",
+      }}
+      width="500"
+    ></canvas>
+  );
 }
+
+export default forwardRef(MemePreview);
