3fe6c49cf474c665869b2f8760808b0c45a0a284
avinashowgre <avinashow.gre@gmail.com>
2023-01-27 22:26:58 -0600

    fixed meme download

--- src/components/MemeContainer.jsx
+++ src/components/MemeContainer.jsx
@@ -75,12 +75,15 @@ export default function MemeContainer() {
   }
 
   function saveMeme() {
-    var win = window.open();
-    win.document.write(
-      '<iframe src="' +
-        canvasRef.current.toDataURL("image/png") +
-        '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
-    );
+    let link = document.createElement("a");
+    link.download = "meme.png";
+    link.href = canvasRef.current
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    link.click();
+    setTimeout(() => {
+      link.remove();
+    }, 1000);
   }
 
   return (
